1d6f0afaa4cf69840674ad9369bf24eb2c5a798b
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-09 13:52:52 -0600

    lunch

--- farm-animal/farm-animal.js
+++ farm-animal/farm-animal.js
@@ -5,8 +5,24 @@ const param = new URLSearchParams(window.location.search);
 
 const farmAnimal = findById(param.get('id'), animals);
 
-const animalName = document.getElementById('animal-name');
-//animalName.textContent = farmAnimal.name;
-console.log('animal js is running');
 
-console.log(farmAnimal);
+const animalName = document.getElementById('animal-name');
+animalName.textContent = farmAnimal.name;
+
+export function () {
+    const div = document.createElement('div');
+    div.classList.add('');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = ;
+
+    const img = document.createElement('img');
+    img.src = pets.image;
+
+    const p = document.createElement('p');
+    p.textContent = `${pets.name} is ${pets.age} and is a ${pets.breed}`;
+
+    }
+    
+    div.append(h1, img, p, ul);
+    return div;
