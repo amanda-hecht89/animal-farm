a5d9575953905ee21a409c86332a27203f51de5b
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-05-09 12:18:40 -0600

    added new html and js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import { animals } from './data.js';
 import { renderListItem } from './render-utils.js';
 
 const main = document.querySelector('main');
+const params = new URLSearchParams
 
 for (let animal of animals) {
     const animalDiv = renderListItem(animal);
---
+++ styles/farm animal/animals.js
@@ -0,0 +1,9 @@
+import { animals } from "../../data.js";
+import { findById } from "../../utils.js";
+
+const param = new URLSearchParams(window.location.search);
+
+const farmAnimal = findById(params.get('id'), animals);
+
+const aniamlName = document.getElementById('animal-name');
+aniamlName.textContent = animals.name;
